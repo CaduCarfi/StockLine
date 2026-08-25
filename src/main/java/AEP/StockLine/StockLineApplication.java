d1356eee7da88648d6502f53d915c15155ba4a3a
CaduCarfi <157553755+CaduCarfi@users.noreply.github.com>
2026-08-24 21:52:28 -0300

    first commit

---
+++ src/main/java/AEP/StockLine/StockLineApplication.java
@@ -0,0 +1,13 @@
+package AEP.StockLine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AEP/StockLine/StockLineApplicationTests.java
@@ -0,0 +1,13 @@
+package AEP.StockLine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
