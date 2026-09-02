de0728dc750f23d304201de99e8b90a432f64bde
ThiagoCHAPECO <guampas@uol.com.br>
2026-09-02 20:05:32 -0300

    feat: atualiza DTOs de medicamento

--- src/main/java/AEP/StockLine/dto/MedicamentoRequestDTO.java
+++ src/main/java/AEP/StockLine/dto/MedicamentoRequestDTO.java
@@ -1,4 +1,27 @@
 package AEP.StockLine.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MedicamentoRequestDTO {
+
+    @NotBlank(message = "O nome do medicamento é obrigatório")
+    private String nome;
+
+    private String descricao;
+
+    @NotNull(message = "A quantidade é obrigatória")
+    @Min(value = 0, message = "A quantidade não pode ser negativa")
+    private Integer quantidade;
+
 }
--- src/main/java/AEP/StockLine/dto/MedicamentoResponseDTO.java
+++ src/main/java/AEP/StockLine/dto/MedicamentoResponseDTO.java
@@ -1,4 +1,21 @@
 package AEP.StockLine.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MedicamentoResponseDTO {
+
+    private String id;
+    private String nome;
+    private String descricao;
+    private Integer quantidade;
+    private LocalDate validade;
+    private String lote;
 }
