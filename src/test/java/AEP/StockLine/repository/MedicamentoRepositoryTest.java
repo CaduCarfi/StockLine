ff7ff0328b4f2e20301793bf15dc896f5b2c368d
Leonardo <leonardocbizerra@gmail.com>
2026-09-03 17:17:40 -0300

    fix: configura ambiente de teste do mongodb

--- src/test/java/AEP/StockLine/repository/MedicamentoRepositoryTest.java
+++ src/test/java/AEP/StockLine/repository/MedicamentoRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.mongodb.test.autoconfigure.DataMongoTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +13,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataMongoTest
+@ActiveProfiles("teste")
 class MedicamentoRepositoryTest {
 
     @Autowired
